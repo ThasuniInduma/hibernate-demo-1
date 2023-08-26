c7d9690bd5aa75b64d610ed392e331aaad9ad57a
Thasuni Induma <thasuniinduma@gmail.com>
2023-08-25 18:29:47 -0800

    Student CRUD

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ import util.SessionFactoryConfiguration;
 public class App {
     public static void main(String[] args) throws Exception {
         StudentRepository repo = new StudentRepository();
-        StudentEntity entity = new StudentEntity();
+        /*StudentEntity entity = new StudentEntity();
         entity.setNic("200045785675");
         StudentName studentName = new StudentName("Stu 1", "Stu Last 1");
         entity.setName(studentName);
@@ -19,7 +19,20 @@ public class App {
         mobiles.add("0771234567");
         mobiles.add("0779874563");
         entity.setMobiles(mobiles);
+
+        repo.saveStudent(entity);*/
+
+        StudentEntity studentEntity = repo.getStudent(3);
+        System.out.println(studentEntity.toString());
+
+        studentEntity.setNic("2000634545");
+        studentEntity.getName().setFirstName("Updated First Name");
+
+        repo.updateStudent(studentEntity);
         
-        repo.saveStudent(entity);
+        studentEntity = repo.getStudent(3);
+        System.out.println(studentEntity.toString());
+
+        repo.deleteStudent(studentEntity);
     }
 }
--- src/repository/StudentRepository.java
+++ src/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package repository;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -20,10 +22,44 @@ public class StudentRepository {
         } catch (Exception e) {
             transaction.rollback();
             return -1;
+
         }
 
         
 
+    }
+
+    public StudentEntity getStudent(Integer id) {
+        StudentEntity studentEntity = session.get(StudentEntity.class, id);
+        return studentEntity;
+    }
+
+    public void updateStudent(StudentEntity studentEntity){
+
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.update(studentEntity);
+            transaction.commit();
+            
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+
+        }
+    }
+
+    public void deleteStudent(StudentEntity studentEntity){
+
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.delete(studentEntity);
+            transaction.commit();
+            
+        } catch (Exception e) {
+            transaction.rollback();
+            throw e;
+
+        }
     }
 
 }
